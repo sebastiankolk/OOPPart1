18bfd76eaa4e080d6061581e20288112029c5375
Opilane TTHK <opilane@tthk.ee>
2021-11-25 09:58:08 +0200

    OOP

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        class Dog
+        {
+            string name;
+            int spots;
+            double happiness;
+
+            public Dog(string _name, int _spots)
+            {
+                name = _name;
+                spots = _spots;
+                happiness = 0;
+                Console.WriteLine($"The dog {name} with {spots} spots has been created");
+            }
+
+            //getters for class properties
+            public string Name
+            {
+                get { return name; }
+            }
+            public int Spots
+            {
+                get { return spots; }
+            }
+            public double Happiness
+            {
+                get { return happiness; }
+            }
+            public void Barks()
+            {
+                Console.WriteLine("Woof-woof!");
+                happiness += 0.2;
+            }
+            public void Tailwag()
+            {
+                Console.WriteLine("wiggle-wiggle");
+                happiness += 0.2;
+            }
+            public void Rename(string newName)
+            {
+                name = newName;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Dog myDog = new Dog("Jax", 12);
+            while (myDog.Happiness != 1)
+            {
+                myDog.Barks();
+            }
+            myDog.Tailwag();
+            Console.WriteLine("Rename your dog");
+            string userInput = Console.ReadLine();
+            myDog.Rename(userInput);
+            Console.WriteLine($"Name: {myDog.Name}");
+            Console.WriteLine($"Level of hapiness: {myDog.Happiness}");
+            
+        }
+    }
+}
---
+++ OOPCat/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace OOPCat
+{
+    class Program
+    {
+        class Cat
+        {
+            string name;
+            string color;
+            double hungriness;
+
+            public Cat(string _name, string _color)
+            {
+                name = _name;
+                color = _color;
+                hungriness = 0;
+                Console.WriteLine($"The cat {name} with the color {color} has been created");
+            }
+            public string Name
+            {
+                get { return name; }
+            }
+            public string Color
+            {
+                get { return color; }
+            }
+            public double Hungriness
+            {
+                get { return hungriness; }
+            }
+            public void Sleep()
+            {
+                Console.WriteLine("'Zzz'");
+                hungriness += 0.2;
+            }
+            public void Meow()
+            {
+                Console.WriteLine("Meow");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Cat myCat = new Cat("Elizabeth", "white");
+            while (myCat.Hungriness != 1)
+            {
+                myCat.Sleep();
+                if(myCat.Hungriness >= 1)
+                {
+                    myCat.Meow();
+                }
+            }
+            Console.WriteLine($"Name: {myCat.Name}");
+            Console.WriteLine($"Level of hungriness: {myCat.Hungriness}");
+        }
+    }
+}
